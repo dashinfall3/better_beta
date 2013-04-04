921f6993303154f2a9b12904e0b8bb128161d6bf
Chris Sell <christophersell3@gmail.com>
2013-04-04 11:00:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterBeta::Application.config.secret_token = '283e3fa3cd0ca0589af5ce22d5b85fb6487e60301b7334f903a251d770793524beca819b4c32c6480765b9df2d75414bc17125db0de0e1151c5cf9e966536c19'
